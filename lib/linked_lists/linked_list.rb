d2a424ffc907dc78bfb3472b651d4f8f0d678c87
Michael Reeves <mreeves68@yahoo.com>
2015-11-27 09:52:03 -0700

    Add Node class and specs for linked list

---
+++ lib/linked_lists/linked_list.rb
@@ -0,0 +1,8 @@
+class Node
+  attr_accessor :data, :next
+
+  def initialize(data, next_node = nil)
+    self.data = data
+    self.next = next_node
+  end
+end
---
+++ spec/linked_lists/linked_list_spec.rb
@@ -0,0 +1,39 @@
+require 'linked_lists/linked_list'
+
+describe Node do
+
+  describe "#initialize" do
+    it "requires data" do
+      expect {Node.new}.to raise_error(ArgumentError)
+      expect {Node.new('foobar')}.not_to raise_error
+    end
+
+    it "defaults next to nil" do
+      node = Node.new('')
+      expect(node.next).to be_nil
+    end
+
+    it "allows setting of next" do
+      node = Node.new('')
+      node2 = Node.new('two', node)
+      expect(node2.next).to be node
+    end
+  end
+
+  describe "attributes" do
+    it "allow reading and writing of :data" do
+      node = Node.new('')
+      expect(node.data).not_to eq 'foobar'
+      node.data = 'foobar'
+      expect(node.data).to eq 'foobar'
+    end
+
+    it "allow reading and writing of :next" do
+      node = Node.new("")
+      expect(node.next).to be_nil
+      node.next = 'dogs'
+      expect(node.next).to eq 'dogs'
+    end
+  end
+
+end
