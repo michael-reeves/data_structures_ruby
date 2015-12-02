0708736c7b2c7da89a640789887509ad62a108aa
Michael Reeves <mreeves68@yahoo.com>
2015-12-02 15:04:22 -0700

    Add LinkedList::DoublyLinkedList#each and add Enumberable mixin. Include specs

--- lib/linked_lists/doubly_linked_list.rb
+++ lib/linked_lists/doubly_linked_list.rb
@@ -1,8 +1,24 @@
+require 'linked_lists/doubly_linked_node'
+
 # this implementation of a DoublyLinkedList is circular.
 # tail.next should always point to the head and
 # head.prev should always point to the tail
 class LinkedList
+
   class DoublyLinkedList
+    include Enumerable
+    
     attr_accessor :head
+
+    def each
+      item = head
+      while item
+        yield item
+        item = item.next
+        break if item == head
+      end
+    end
+
   end
+
 end
--- spec/linked_lists/doubly_linked_list_spec.rb
+++ spec/linked_lists/doubly_linked_list_spec.rb
@@ -12,4 +12,29 @@ describe LinkedList::DoublyLinkedList do
     end
   end
 
+  describe "#each" do
+    it "iterates over the list" do
+      node = LinkedList::DoublyLinkedNode.new(1)
+      node.next = node
+      node.prev = node
+
+      subject.head = node
+
+      expect{ subject.each{ |n| puts n.data } }.to output("1\n").to_stdout
+    end
+  end
+
+  it "mixes in Enumerable" do
+    node1 = LinkedList::DoublyLinkedNode.new(1)
+    node2 = LinkedList::DoublyLinkedNode.new(2, node1)
+    node3 = LinkedList::DoublyLinkedNode.new(3, node2, node1)
+    node1.prev = node3
+    node1.next = node2
+    node2.next = node3
+
+    subject.head = node1
+
+    expect(subject.map{ |n| n.data }).to eq [1,2,3]
+  end
+
 end
